72e64598d47fcaa78f98cec8e7bdd979a80b1e3a
Steven21166 <adrians.arellano@unach.edu.ec>
2026-06-07 21:01:42 -0500

    Agregar script start para Render

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,14 +1,27 @@
-const mongoose = require('mongoose')
+const dns = require("dns");
 
-//cadena de conexion 
+// Forzar DNS públicos
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const mongoose = require("mongoose");
+
+//cadena de conexion
 const URI = process.env.MONGODB_URI
-            ?process.env.MONGODB_URI
-            : 'mongodb://localhost/dbtest'
+  ? process.env.MONGODB_URI
+  : "mongodb://localhost/dbtest";
 
-mongoose.connect(URI)
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("✅ MongoDB conectado");
+  })
+  .catch((err) => {
+    console.error("❌ Error MongoDB:");
+    console.error(err);
+  });
 
 const connection = mongoose.connection;
 
-connection.once('open', ()=>{
-    console.log('La base de datos ha sido conectada: ', URI);
-})
+connection.once("open", () => {
+  console.log("La base de datos ha sido conectada: ", URI);
+});
