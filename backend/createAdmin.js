ed6c3796e89809583d6d83dff3520200f78836f8
Steven21166 <adrians.arellano@unach.edu.ec>
2026-06-08 15:46:34 -0500

    Integracion Cloudinary

--- backend/createAdmin.js
+++ backend/createAdmin.js
@@ -1,4 +1,10 @@
 require("dotenv").config();
+
+const dns = require("dns");
+
+// Forzar DNS públicos
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const mongoose = require("mongoose");
 const Usuario = require("./src/models/Usuario");
 
